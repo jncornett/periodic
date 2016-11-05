9eddcd0d4e3cdc0487a28b428b97f08bc21be01f
Joel Cornett <joel.cornett@gmail.com>
2016-11-05 10:52:51 -0400

    added periodic and periodic tests

---
+++ periodic.go
@@ -0,0 +1,42 @@
+// Package periodic contains utilities to run periodic tasks
+package periodic
+
+import "time"
+
+// Serve asynchronously runs a task periodically until it is cancelled.
+// To cancel Serve, send to the cancel channel.
+// If cancel is nil, a new buffered channel of size 1 is created.
+// Serve returns the cancel channel.
+func Serve(period time.Duration, task func(), cancel chan bool) chan bool {
+	if cancel == nil {
+		cancel = make(chan bool, 1)
+	}
+	go func() {
+		for {
+			if !SleepOrCancel(period, cancel) {
+				return
+			}
+			task()
+		}
+	}()
+	return cancel
+}
+
+// SleepOrCancel will block until interval passes, or cancel is read from.
+// To cancel the sleep, simply send a value to cancel. Cancel can be nil,
+// in which case the caller will not be able to cancel the sleep.
+// SleepOrCancel returns true if it completed sleeping,
+// and returns false if it was cancelled.
+func SleepOrCancel(interval time.Duration, cancel <-chan bool) bool {
+	timeout := make(chan bool)
+	go func() {
+		time.Sleep(interval)
+		timeout <- true
+	}()
+	select {
+	case <-timeout:
+		return true
+	case <-cancel:
+		return false
+	}
+}
---
+++ periodic_test.go
@@ -0,0 +1,71 @@
+package periodic_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/jncornett/periodic"
+)
+
+func TestSleepOrCancelTimesOut(t *testing.T) {
+	// test without cancel provided
+	if !periodic.SleepOrCancel(time.Millisecond, nil) {
+		t.Error("expected SleepOrCancel to return true")
+	}
+	// test with cancel provided
+	if !periodic.SleepOrCancel(time.Millisecond, make(chan bool)) {
+		t.Error("expected SleepOrCancel to return true")
+	}
+}
+
+func TestSleepOrCancelGetsCancelled(t *testing.T) {
+	cancel := make(chan bool)
+	go func() { cancel <- true }()
+	if periodic.SleepOrCancel(time.Minute, cancel) {
+		t.Error("expected SleepOrCancel to return false")
+	}
+}
+
+func TestServeCanBeCancelled(t *testing.T) {
+	var touched bool
+	cancel := make(chan bool) // need this to block
+	periodic.Serve(time.Minute, func() { touched = true }, cancel)
+	cancel <- true
+	if touched {
+		t.Error("expected touched to be false")
+	}
+}
+
+func TestServeReturnsTheSameChannel(t *testing.T) {
+	cancel := make(chan bool)
+	rv := periodic.Serve(time.Minute, func() {}, cancel)
+	if rv != cancel {
+		t.Error("expected channels to be equal")
+	}
+}
+
+func TestServeMakesANewChannelOnNilArg(t *testing.T) {
+	cancel := periodic.Serve(time.Minute, func() {}, nil)
+	if cancel == nil {
+		t.Error("expected cancel to not be nil")
+	}
+}
+
+func TestServeRunsTask(t *testing.T) {
+	var touched bool
+	cancel := make(chan bool, 1)
+	done := make(chan bool, 1)
+	periodic.Serve(
+		time.Millisecond,
+		func() {
+			touched = true
+			cancel <- true
+			done <- true
+		},
+		cancel,
+	)
+	<-done
+	if !touched {
+		t.Error("expected touched to be true")
+	}
+}
